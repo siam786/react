8765bb44ef14baefc72686ee69f64e0372395fac
siam786 <shohrab.hossain36@gmail.comm>
2021-05-30 12:15:37 +0600

    ad

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Home from "./components/Home"
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Header />
+      <Header />
+      <Header />
+      <Main />
+      <Main/>
+      <Header />
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
